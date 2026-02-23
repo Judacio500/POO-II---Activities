69f4121d2b1e92bf4e185a2ba795dbd2a8b037ab
Judacio500 <darkiman18@gmail.com>
2026-02-22 21:08:15 -0600

    Final

--- quicksort/src/main/java/com/genericorder/Main.java
+++ quicksort/src/main/java/com/genericorder/Main.java
@@ -1,9 +1,22 @@
 package com.genericorder;
 
-public class Main 
-{
-    public static void main(String[] args) 
-    {
+public class Main {
+    public static void main(String[] args) {
+        
+        // 1. Prueba con Enteros (int[])
+        System.out.println("--- Prueba con Integer ---");
+        int[] intVector = { 101, 99, 12, 19, 21, 111, 345, 25, 77, 81 };
+        Number[] resultInt = Utils.ordena(intVector);
+        Utils.imprimir(resultInt);
 
+        // 2. Prueba con Flotantes (float[])
+        System.out.println("\n--- Prueba con Float ---");
+        float[] floatVector = { 19.1f, 456.6f, 23.45f, 12.34f, 11.11f, 354.55f, 78.45f, 28.33f, 45.99f, 108.88f };
+        Number[] resultFloat = Utils.ordena(floatVector);
+        Utils.imprimir(resultFloat);
+
+        String [] strVector = {"zz", "aa", "cc", "hh", "bb", "ee", "ll"};
+        // String [] resultStr = Utils.ordena(strVector); 
+        // Descomentar para ver error
     }
 }
--- quicksort/src/main/java/com/genericorder/Utils.java
+++ quicksort/src/main/java/com/genericorder/Utils.java
@@ -1,6 +1,71 @@
 package com.genericorder;
+import java.lang.reflect.Array;
 
-public class Utils 
-{
+public class Utils {
+
+    public static <T extends Number & Comparable<T>> T[] ordena(Object primArr)    
+    {
+        int length = java.lang.reflect.Array.getLength(primArr);
     
+        T[] objArr = (T[]) new Number[length];
+
+        for (int i = 0; i < length; i++) 
+        {
+            Object element = java.lang.reflect.Array.get(primArr, i);
+            objArr[i] = (T) element;
+        }
+
+        QuickSort((T[])objArr, 0, length-1);
+
+        return objArr;
+    }
+
+    private static <T extends Number & Comparable<T>> void QuickSort(T[] arr, int low, int high) 
+    {
+        if(low>=high)
+        {
+            return;
+        }
+
+        int p = partition(arr, low, high);
+        QuickSort(arr, low, p-1);
+        QuickSort(arr, p+1, high);
+        return;
+    }
+
+    private static <T extends Number & Comparable<T>> int partition(T[] arr, int low, int high)
+    {
+        int i = low, j =high-1, pivot = high;
+
+        while(i<=j)
+        {
+            while(i<high && arr[i].compareTo(arr[pivot])<0)
+                i++;
+            while(j>=low && arr[j].compareTo(arr[pivot])>0)
+                j--;
+
+            if(i<=j)
+            {
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        T temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    }
+
+    public static <T extends Number> void imprimir(T[] arr) 
+    {
+        for (T elemento : arr) 
+        {
+            System.out.print(elemento + " ");
+        }
+        System.out.println();
+    }   
 }
